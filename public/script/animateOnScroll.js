6df8b6116974429301665eef1a51cf7c5a8e208b
davefoxxo <kochanskidavid9@gmail.com>
2023-03-03 09:23:11 -0500

    Add cool animations

---
+++ public/script/animateOnScroll.js
@@ -0,0 +1,27 @@
+// TODO
+
+const skillBars = document.querySelectorAll(".skill");
+
+const mySkillObserver = new IntersectionObserver((skillElements) => {
+    skillElements.forEach((element) => {
+        if(element.isIntersecting) {
+            element.target.classList.add("show-skill");
+        }
+    })
+})
+
+skillBars.forEach((element) => mySkillObserver.observe(element));
+
+
+const aboutBars = document.querySelectorAll(".about-bar");
+
+const myAboutObserver = new IntersectionObserver((aboutElements) => {
+    aboutElements.forEach((element) => {
+        if(element.isIntersecting) {
+            element.target.classList.add("show-about");
+        }
+    })
+})
+
+aboutBars.forEach((element) => myAboutObserver.observe(element));
+
