3ae2f73d286937d381da73ed58c9b63c663f7ba5
davefoxxo <kochanskidavid9@gmail.com>
2023-03-22 15:39:31 -0400

    Code cleanup

--- public/script/interactions.js
+++ public/script/interactions.js
@@ -2,14 +2,19 @@ const btn = document.getElementById("interaction-button");
 const icon = document.querySelector("#interaction-button > i");
 const wrapper = document.querySelector(".interactions-wrapper");
 const inters = document.querySelectorAll(".interaction");
-let active = false;
-let numShown = 3;
+
 const SHOW_DELAY = 150;
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 let media = window.matchMedia("(max-width: 768px)");
+let numShown = 3;
+let active = false;
 
+// Calculate shown elements when page is initially loaded..
+document.body.onload = e => checkForAndApplyShown();
 
+// And also whenever the window size is changed,
+// that could cause a potential change in num of elements shown
+window.onresize = e => checkForAndApplyShown();
 
 const checkForAndApplyShown = () => {
     media = window.matchMedia("(max-width: 768px)");
@@ -22,17 +27,9 @@ const checkForAndApplyShown = () => {
 
         let tail = Array.from(inters).slice(numShown);
         tail.forEach((element) => { element.classList.add("interaction-hide")})
-    
     }
-
 }
 
-// Calculate shown elements when page is initially loaded..
-document.body.onload = e => checkForAndApplyShown();
-
-// And also whenever the window size is changed,
-// that could cause a potential change in num of elements shown
-window.onresize = e => checkForAndApplyShown();
 
 btn.onmousedown = e => {
     active = !active;
@@ -50,8 +47,5 @@ btn.onmousedown = e => {
             element.classList.toggle("interaction-hide");
 
         }, idx * SHOW_DELAY)
-    })
-
-    
-    
+    })   
 }
