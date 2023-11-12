e09a4151c575476387c49c2f153ca01ac3a227b0
davidkochanski <kochanskidavid9@gmail.com>
2023-11-12 16:19:29 -0500

    Updates

--- script/parallax.js
+++ script/parallax.js
@@ -27,11 +27,11 @@ function parallaxify(element) {
 
     if (scrollPosition >= windowHeight) {
       element.style.position = 'absolute';
-      element.style.top = 1.25 * windowHeight + 'px';
+      element.style.top = 1.50 * windowHeight + 'px';
     }
     
     // When the user scrolls back up, make the element fixed again
-    if (scrollPosition < 1.25 * windowHeight) {
+    if (scrollPosition < 1.50 * windowHeight) {
       element.style.position = 'fixed';
       element.style.top = '0px';
     }
